8b3eade8b2d772022d6246aeeda8fe5c58858d83
ceng3011 <ceng3011@gmail.com>
2024-08-02 09:01:47 +0000

    add main header styling

--- components/main-header.js
+++ components/main-header.js
@@ -2,15 +2,27 @@ import Link from "next/link";
 
 export default function MainHeader() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/news">News</Link>
-        </li>
-      </ul>
-    </nav>
+    <header className="flex justify-between items-center mb-8">
+      <div className="font-sans text-xl rounded-md">
+        <Link
+          href="/"
+          className="text-[#e1e1d7] hover:text-[#e1e1d7] no-underline"
+        >
+          NextNews
+        </Link>
+      </div>
+      <nav>
+        <ul className="flex gap-8 font-bold">
+          <li>
+            <Link
+              href="/news"
+              className="text-[#e5e5e1] px-2 py-1 rounded-md no-underline hover:bg-[#bcbcb7] hover:text-[#181817] active:bg-[#bcbcb7] active:text-[#181817]"
+            >
+              News
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
