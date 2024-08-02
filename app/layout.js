96c5d12eea50bcda6153c1e524b80567d48f7c50
ceng3011 <ceng3011@gmail.com>
2024-08-02 09:02:25 +0000

    add layout div for styling

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,20 @@
-import MainHeader from '@/components/main-header';
-import './globals.css';
+import MainHeader from "@/components/main-header";
+import "./globals.css";
 
 export const metadata = {
-  title: 'Next.js Page Routing & Rendering',
-  description: 'Learn how to route to different pages.',
-}
+  title: "Next.js Page Routing & Rendering",
+  description: "Learn how to route to different pages.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <MainHeader />
-        {children}
+        <div className="w-5/6 max-w-4xl mx-4 my-auto">
+          <MainHeader />
+          {children}
+        </div>
       </body>
     </html>
-  )
+  );
 }
