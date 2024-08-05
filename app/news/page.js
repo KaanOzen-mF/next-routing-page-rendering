c0b64739d08218e009d31daaca24734830bd8d0d
ceng3011 <ceng3011@gmail.com>
2024-08-05 14:25:41 +0000

    add news page dummy data

--- app/news/page.js
+++ app/news/page.js
@@ -1,21 +1,24 @@
 import Link from "next/link";
 
+import { DUMMY_NEWS } from "@/dummy-news";
+
 export default function NewsPage() {
   return (
     <>
-      <h1>News Page</h1>
-
-      <Link href="/news/One">
-        <p>New One</p>
-      </Link>
-
-      <Link href="/news/Two">
-        <p>New Two</p>
-      </Link>
-
-      <Link href="/news/Three">
-        <p>New Three</p>
-      </Link>
+      <h1 className="text-4xl">News Page</h1>
+      <ul className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(15rem,1fr))]">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
