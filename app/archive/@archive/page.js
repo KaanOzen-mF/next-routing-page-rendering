e1f49d60b670aa143169a919a4832b461ded4590
ceng3011 <ceng3011@gmail.com>
2024-08-06 08:50:33 +0000

    add archieve page functionallity

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,3 +1,21 @@
-export default function ArchievePage() {
-  return <h1>Archieve Page</h1>;
+import Link from "next/link";
+
+import { getAvailableNewsYears } from "@/lib/news";
+
+export default function ArchivePage() {
+  const links = getAvailableNewsYears();
+
+  return (
+    <header>
+      <nav>
+        <ul className="flex">
+          {links.map((link) => (
+            <li className="mr-4" key={link}>
+              <Link href={`/archive/${link}`}>{link}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
